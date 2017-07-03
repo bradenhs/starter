3190c73b631f36068ca704f0ef84e067ee8b2576
Braden Snell <bhsnll@gmail.com>
2017-07-03 14:53:58 -0600

    adds prettier

--- scripts/pre_commit_hook.sh
+++ scripts/pre_commit_hook.sh
@@ -1,3 +1,3 @@
 echo "Formatting Staged Files:"
 git diff --staged --diff-filter=dx --name-only HEAD | grep "^src\/.*[.]tsx\?$" | xargs -I % sh -c 'prettier --write --no-semi --single-quote --jsx-single-quote --space-before-function-paren %; git add %'
-printf "Done"
+printf "Done\n"
